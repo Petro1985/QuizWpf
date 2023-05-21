7338605ff9262d0062ee811a99c591750fb95f25
Petr <Petro1985p@gmail.com>
2023-05-21 22:30:37 +0300

    Forgotten files

---
+++ ViewModels/ViewModel/ResultVm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Model.Services;
+
+namespace ViewModels.ViewModel;
+
+public class ResultVm : INotifyPropertyChanged
+{
+    private readonly QuizEngine _quizEngine;
+    
+    private List<QuizResult> _results;
+    public List<QuizResult> Results
+    {
+        get => _results;
+        set
+        {
+            _results = value;
+            OnPropertyChanged();
+        }
+    }
+
+
+    private string _scoreText;
+    public string ScoreText
+    {
+        get => _scoreText;
+        set
+        {
+            _scoreText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ResultVm(QuizEngine quizEngine)
+    {
+        _quizEngine = quizEngine;
+        _quizEngine.OnQuizEnded += OnQuizEnded;
+        Results = _quizEngine.GetResults().ToList();
+    }
+
+    private void OnQuizEnded(Object? o, EventArgs e)
+    {
+        Results = _quizEngine.GetResults().ToList();
+        ScoreText = $"Your score is {_quizEngine.GetScore()}";
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ Views/Windows/ResultWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Model.Services.ViewService;
+using ViewModels.ViewModel;
+
+namespace TestMVVM.Windows
+{
+    public partial class ResultWindow : UserControl, IView
+    {
+        private readonly IViewsLoader _viewsLoader;
+        public ResultWindow(ResultVm resultVm, IViewsLoader viewsLoader)
+        {
+            _viewsLoader = viewsLoader;
+            DataContext = resultVm;
+            InitializeComponent();
+        }
+
+        private void ToMainMenu_Click(object sender, RoutedEventArgs e)
+        {
+            _viewsLoader.LoadView<MainMenuWindow>();
+        }
+    }
+}
+
